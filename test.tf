e9866d7f0be93064fd440fed45ed756d59e2bc70
zainab <akinlawonjoyz@gmail.com>
2023-04-04 23:15:05 +0000

    terraform code to provision AWS infrastructure

---
+++ test.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "test" {
+  ami           = "ami-0aaa5410833273cfe"
+  instance_type = "t2.micro" 
+  subnet_id     = aws_subnet.subnet-a.id 
+  associate_public_ip_address = "true" 
+  key_name = "my-keypair" 
+  vpc_security_group_ids = ["${aws_security_group.my-sg.id}"]
+
+  tags = {
+    Name = "test" 
+  } 
+} 
